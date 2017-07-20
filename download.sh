d0bad120887a206e0057bb5c6ba7c56e8e46eb9e
Jeremy Hsu <jeremycchsu@gmail.com>
2017-07-20 11:09:21 +0800

    Update `download.sh`

--- download.sh
+++ download.sh
@@ -1,11 +1,9 @@
-mkdir dataset
+mkdir -p dataset/vcc2016/wav
 cd dataset
-mkdir wav
-wget http://datashare.is.ed.ac.uk/download/10283/2042/SUPERSEDED_-_The_Voice_Conversion_Challenge_2016.zip
-unzip SUPERSEDED_-_The_Voice_Conversion_Challenge_2016.zip -d vcc2016
-cd vcc2016
+wget "http://datashare.is.ed.ac.uk/download/10283/2042/SUPERSEDED_-_The_Voice_Conversion_Challenge_2016.zip"
+unzip SUPERSEDED_-_The_Voice_Conversion_Challenge_2016.zip vcc2016_training.zip evaluation_all.zip
 unzip vcc2016_training.zip
-unzip evaluation_all.zip
-mv vcc2016_training "../wav/Training Set"
-mv evaluation_all "../wav/Testing Set"
-cd ../..
+mv vcc2016_training "./vcc2016/wav/Training Set"
+unzip evaluation_all.zip -d "vcc2016/wav/Testing Set"
+rm evaluation_all.zip vcc2016_training.zip
+cd ..
