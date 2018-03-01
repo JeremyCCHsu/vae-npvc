6e439453d2b9728e11aa8b512d7f70d75fe77aa3
Jeremy Hsu <jeremycchsu@gmail.com>
2018-03-01 14:22:20 -0800

    update download

--- download.sh
+++ download.sh
@@ -1,10 +1,14 @@
 datadir="dataset/vcc2016/wav/"
 datalink="https://datashare.is.ed.ac.uk/bitstream/handle/10283/2211/"
+
 mkdir -p $datadir
+
 wget "${datalink}license_text"
+
 wget "${datalink}vcc2016_training.zip"
-wget "${datalink}evaluation_all.zip"
 unzip vcc2016_training.zip
-unzip evaluation_all.zip
 mv vcc2016_training "${datadir}Training Set"
+
+wget "${datalink}evaluation_all.zip"
+unzip evaluation_all.zip
 mv evaluation_all "${datadir}Testing Set"
